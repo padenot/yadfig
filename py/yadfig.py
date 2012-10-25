ee4ef59a0a3aa0eb5c590affefe7a9945d73a9e0
Benjamin BOUVIER <bnjbouv+github@gmail.com>
2012-10-25 00:40:02 -0400

    Putting the generation processus into a class, all global variables become attributes.

--- py/yadfig.py
+++ py/yadfig.py
@@ -28,102 +28,118 @@ class bcolors:
         self.ENDC = ''
         self.BOLD = ''
 
-PLACE=""
-TITLE=""
-ROWCOUNT=4
+DEFAULT_ROWCOUNT=4
 template='''__TEMPLATE__'''
 THUMB_DIR=".c"
 
-images = list()
+class Generator:
+    def __init__(self, dirname, title = "", place = None, rowcount = DEFAULT_ROWCOUNT):
+        self.dirname = dirname
+        self.rowcount = rowcount
+        self.place = place
+        self.title = title
+        self.images = []
 
-def is_image(mime):
-  if mime.find("image") == 0:
-    return True
-  return False
+    def run(self):
+        # TODO stop if error
+        self.list_files()
+        self.images.sort()
+        self.create_thumbs()
+        self.output_html()
 
-def list_files(dirname):
-    try:
-        os.stat(dirname)
-        full = os.path.join(dirname, THUMB_DIR)
-        if not os.path.exists(full):
-            os.makedirs(full)
-    except os.error, err:
-        print bcolors.FAIL + str(err) + bcolors.ENDC
-        exit(1)
+        print bcolors.OKGREEN + "Generation successful:"
+        print "\t" + os.path.join(self.dirname,"index.html")
+        print "\t" + os.path.join(self.dirname,THUMB_DIR)+ " for " + str(len(self.images)) + " pictures." + bcolors.ENDC
 
-    filenames = os.listdir(dirname)
+    def is_image(self, mime):
+      # TODO return expression
+      if mime.find("image") == 0:
+        return True
+      return False
 
-    for name in filenames:
-        full_name = os.path.join(dirname, name)
-        print name
-        if is_image(str(mimetypes.guess_type(full_name)[0])):
-            metadata = pyexiv2.ImageMetadata(full_name)
-            metadata.read()
-            images.append([name, metadata])
-    if len(images) == 0:
-        print bcolors.WARNING + "No pictures found in "+dirname+", exiting." + bcolors.ENDC
-        exit(1)
+    def list_files(self):
+        try:
+            os.stat(self.dirname)
+            full = os.path.join(self.dirname, THUMB_DIR)
+            if not os.path.exists(full):
+                os.makedirs(full)
+        except os.error, err:
+            print bcolors.FAIL + str(err) + bcolors.ENDC
+            # TODO continue boolean
+            exit(1)
 
+        filenames = os.listdir(self.dirname)
 
-def get_pictures():
-  i = 0
-  out = ""
-  out+='<div class="row">'
-  for image in images:
-    if i % ROWCOUNT == 0:
-      out+='</div>'
+        for name in filenames:
+            full_name = os.path.join(self.dirname, name)
+            print name
+            if self.is_image(str(mimetypes.guess_type(full_name)[0])):
+                metadata = pyexiv2.ImageMetadata(full_name)
+                metadata.read()
+                self.images.append([name, metadata])
+        if len(self.images) == 0:
+            print bcolors.WARNING + "No pictures found in "+self.dirname+", exiting." + bcolors.ENDC
+            exit(1) # TODO continue boolean
+
+    def get_pictures(self):
+      i = 0
+      out = ""
       out+='<div class="row">'
-    out+='<div class="cell"><img class="thumb" src=".c/'
-    out+=image[0]
-    out+='"><p>'
-    out+=os.path.basename(image[0])
-    out+='</p></div>'
-    i=i+1
-  out+="</div>"
-  return out
+      for image in self.images:
+        if i % self.rowcount == 0:
+          out+='</div>'
+          out+='<div class="row">'
+        out+='<div class="cell"><img class="thumb" src=".c/'
+        out+=image[0]
+        out+='"><p>'
+        out+=os.path.basename(image[0])
+        out+='</p></div>'
+        i=i+1
+      out+="</div>"
+      return out
 
-def get_date_interval():
-    dates = list()
-    for i in images:
-      try:
-        dates.append(i[1]['Exif.Image.DateTime'].value)
-      except KeyError:
-        continue
-    if len(dates) == 0:
-      print bcolors.WARNING + "No dates found in EXIF metadata. Disabling date output" + bcolors.ENDC
-      return "";
+    def get_date_interval(self):
+        dates = list()
+        for i in self.images:
+          try:
+            dates.append(i[1]['Exif.Image.DateTime'].value)
+          except KeyError:
+            continue
+        if len(dates) == 0:
+          print bcolors.WARNING + "No dates found in EXIF metadata. Disabling date output" + bcolors.ENDC
+          return "";
 
-    dates.sort()
-    formatdate = "%d %B %Y"
-    formattime = "%H:%M"
-    formatfull = formatdate + " at " + formattime
-    date1 = dates[0].strftime(formatdate)
-    date2 = dates[1].strftime(formatdate)
-    if date1 == date2:
-      return "The " + dates[0].strftime(formatdate) + ", from " + dates[0].strftime(formattime) + " to " + dates[-1].strftime(formattime)
-    else:
-      return "From the " + dates[0].strftime(formatfull) + " to the " + dates[-1].strftime(formatfull)
+        dates.sort()
+        formatdate = "%d %B %Y"
+        formattime = "%H:%M"
+        formatfull = formatdate + " at " + formattime
+        date1 = dates[0].strftime(formatdate)
+        date2 = dates[1].strftime(formatdate)
+        if date1 == date2:
+          return "The " + dates[0].strftime(formatdate) + ", from " + dates[0].strftime(formattime) + " to " + dates[-1].strftime(formattime)
+        else:
+          return "From the " + dates[0].strftime(formatfull) + " to the " + dates[-1].strftime(formatfull)
 
-def output_html(dirname):
-    global template
-    template = template.replace('__TITLE__', TITLE)
-    template = template.replace('__PLACE__', PLACE)
-    if not PLACE:
-      template = template.replace('__DATE__', get_date_interval())
-    else:
-      template = template.replace('__DATE__', get_date_interval()+',')
-    pics = get_pictures()
-    template = template.replace('__GALLERY__', pics);
-    f = open(os.path.join(dirname, "index.html"), "w")
-    f.write(template)
-    f.close()
+    def output_html(self):
+        global template
+        ctemplate = template.replace('__TITLE__', self.title)
+        ctemplate = ctemplate.replace('__PLACE__', self.place)
+        if not self.place:
+          ctemplate = ctemplate.replace('__DATE__', self.get_date_interval())
+        else:
+          ctemplate = ctemplate.replace('__DATE__', self.get_date_interval()+',')
+        pics = self.get_pictures()
+        ctemplate = ctemplate.replace('__GALLERY__', pics);
+        f = open(os.path.join(self.dirname, "index.html"), "w")
+        f.write(ctemplate)
+        f.close()
 
-def create_thumbs(dirname):
-    for i in images:
-        im = Image.open(os.path.join(dirname, i[0]))
-        im.thumbnail([im.size[0]/4, im.size[0]/4], Image.ANTIALIAS)
-        thumb = os.path.join(dirname, THUMB_DIR, os.path.basename(i[0]))
-        im.save(thumb)
+    def create_thumbs(self):
+        for i in self.images:
+            im = Image.open(os.path.join(self.dirname, i[0]))
+            im.thumbnail([im.size[0]/4, im.size[0]/4], Image.ANTIALIAS) # TODO thumbnail with limited size
+            thumb = os.path.join(self.dirname, THUMB_DIR, os.path.basename(i[0]))
+            im.save(thumb)
 
 
 def usage():
@@ -134,7 +150,7 @@ def usage():
     print "\t-d : a directory to operate on."
 
 def main():
-    global TITLE, PLACE
+    title, place = "", ""
     try:
         arg, opts = getopt.getopt(sys.argv[1:], "hp:t:d:")
     except getopt.GetoptError, err:
@@ -146,23 +162,19 @@ def main():
 
     for o, a in arg:
         if o in "-p":
-            PLACE=a
+            place=a
         elif o in "-h":
             usage
             sys.exit
         elif o in "-t":
-            TITLE=a
+            title=a
         elif o in "-d":
             DIR=a
         else:
             assert False, "unhandled option"
-    list_files(DIR)
-    images.sort()
-    create_thumbs(DIR)
-    output_html(DIR)
-    print bcolors.OKGREEN + "Generation successful:"
-    print "\t" + os.path.join(DIR,"index.html")
-    print "\t" + os.path.join(DIR,THUMB_DIR)+ " for " + str(len(images)) + " pictures." + bcolors.ENDC
+    
+    g = Generator(dirname = DIR, title = title, place = place)
+    g.run()
 
 if __name__ == "__main__":
     main()
